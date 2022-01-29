996f36db590a3ef67df68e756453d0102c37feac
Ibrah <brahim.oued@gmail.com>
2022-01-29 22:08:46 +0000

    add model evaluation stage for logistic regression

--- scripts/config.py
+++ scripts/config.py
@@ -12,3 +12,4 @@ class Config:
     MODELS_PATH = ASSETS_PATH / "models"
     MODELS_V2_PATH = ASSETS_PATH / "models_v2"
     METRICS_FILE_PATH = ASSETS_PATH / "metrics.json"
+    METRICS_V2_FILE_PATH = ASSETS_PATH / "metrics_v2.json"
--- scripts/eval_model.py
+++ scripts/eval_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 from config import Config
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import StandardScaler
-Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
+Config.MODELS_V2_PATH.mkdir(parents=True, exist_ok=True)
 
 
 df_x_test = pd.read_csv(str(Config.FEATURES_PATH / "test_features.csv"))
@@ -17,12 +17,12 @@ X_train = scaler.fit_transform(df_x_test)
 y_true = df_y_test.pop('Class')
 
 # Chargement du modèle
-model = pickle.load(open(str(Config.MODELS_PATH/"model.pk"), mode='rb'))
+model = pickle.load(open(str(Config.MODELS_V2_PATH/"model_v2.pk"), mode='rb'))
 # Prediction
 y_pred = model.predict(X_train)
 accuracy = accuracy_score(y_true, y_pred)
 precision = precision_score(y_true, y_pred, average='micro')
 recall_score = recall_score(y_true, y_pred, average='micro')
 f1_score = f1_score(y_true, y_pred, average='micro')
-with open(str(Config.METRICS_FILE_PATH), mode='w') as f:
+with open(str(Config.METRICS_V2_FILE_PATH), mode='w') as f:
     json.dump(dict(accuracy=accuracy, precision=precision, f1_score=f1_score), f)
